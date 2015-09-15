5af585c398140c1b268e613b844718b6a552a6eb
Rajur <rajur.hint@gmail.com>
2015-09-15 14:51:42 +0100

    tabel+ pilt lisatud

--- index.php
+++ index.php
@@ -11,20 +11,42 @@
 <h5>Between all the takeovers, Google has still found enough time to develop its own smartwatch together with LG.</h5>
     <img src="http://sproutmoney.com/files/2014/02/smartwatch.png"/>
         <br>
-            <table border="1"><!--tabeli algus-->
+            <h3>Smartwatch review</h3>
+        <table border="1"><!--tabeli algus-->
                 <tr>
                     <th></th>
-                    <th></th>
-                    <th></th>
+                    <th>The basic Apple Watch</th>
+                    <th>Samsung Gear Live</th>
+                    <th>Sony SmartWatch 3</th>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>Description</td>
+                    <td>Apple Watch is good, but better suited on the
+                        wrists of early adopters and boutique shop
+                        regulars. It's convenient but there's a learning
+                        curve you have to overcome and a high price that
+                        some people won't be able to get around.</td>
+                    <td>The Gear Live has a clean metal body, more features for the price than the LG watch,
+                        works with many Android phones (4.3 or later), has impressive Google Voice recognition,
+                        and could have an interesting variety of apps sooner than later. Terrible battery life;
+                        a display that doesn't work in bright sunlight; a strange card-based notification system
+                        that doesn't always pop up with the info you want; annoying snap-on charger that's not easy to attach.</td>
+                    <td>The Sony SmartWatch 3 might not be the best-looking Android Wear device so far –
+                        that honour belongs to the LG G Watch R – but it’s by far the most useful. Its
+                        design may be pretty uninspiring and the display is fairly mediocre but the
+                        functionality afforded by the GPS is unrivalled in the Google smartwatch arena
+                        and the battery life is far better than we’ve seen on any of the Sony’s rivals.</td>
                 </tr>
                 <tr>
+                    <td>Features</td>
+                    <td>Size: 42mm</td>
                     <td></td>
-                    <td></td>
+                    <td>Packs in GPS connectivity; sporty look</td>
+                </tr>
+                <tr>
+                    <td>Price</td>
+                    <td>$349</td>
+                    <td>$149.99</td>
                     <td></td>
                 </tr>
         </table>
