3c8b80c3162e05ee632d6c2694907b36744bd683
Rajur <rajur.hint@gmail.com>
2015-09-15 10:55:12 +0100

    tabel+ pilt lisatud

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE>
+    <html>
+        <head>
+            <title>Kõikide tag'ide projekt</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+        </head>
+        <body>
+<h1>Smartwatch</h1>
+    <p>On this site you will be able to find some information about smartwatches</p>
+        <br>
+<h5>Between all the takeovers, Google has still found enough time to develop its own smartwatch together with LG.</h5>
+    <img src="http://sproutmoney.com/files/2014/02/smartwatch.png"/>
+        <br>
+            <table border="1"><!--tabeli algus-->
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+        </table>
+
+
+
+
+</body>
