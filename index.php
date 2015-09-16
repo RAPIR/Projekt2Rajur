2ae4230adcf1bc4ac1e0e5bc243fe96606071a58
Rajur <rajur.hint@gmail.com>
2015-09-16 23:05:01 +0100

    video lisatud + bg lis

--- index.php
+++ index.php
@@ -2,56 +2,94 @@
     <html>
         <head>
             <title>Kõikide tag'ide projekt</title>
-    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+            <link rel="stylesheet" type="text/css" href="stylesheet.css">
         </head>
         <body>
-<h1>SMARTWATCH</h1>
-    <p>On this site you will be able to find some information about smartwatches</p>
-        <br>
-<h5>Between all the takeovers, Google has still found enough time to develop its own smartwatch together with LG.</h5>
-    <img src="http://sproutmoney.com/files/2014/02/smartwatch.png"/>
-        <br>
-            <h3>Smartwatch review</h3>
-        <table border="1"><!--tabeli algus-->
-                <tr>
-                    <th></th>
-                    <th>The basic Apple Watch</th>
-                    <th>Samsung Gear Live</th>
-                    <th>Sony SmartWatch 3</th>
-                </tr>
-                <tr>
-                    <td>Description</td>
-                    <td>Apple Watch is good, but better suited on the
-                        wrists of early adopters and boutique shop
-                        regulars. It's convenient but there's a learning
-                        curve you have to overcome and a high price that
-                        some people won't be able to get around.</td>
-                    <td>The Gear Live has a clean metal body, more features for the price than the LG watch,
-                        works with many Android phones (4.3 or later), has impressive Google Voice recognition,
-                        and could have an interesting variety of apps sooner than later. Terrible battery life;
-                        a display that doesn't work in bright sunlight; a strange card-based notification system
-                        that doesn't always pop up with the info you want; annoying snap-on charger that's not easy to attach.</td>
-                    <td>The Sony SmartWatch 3 might not be the best-looking Android Wear device so far –
-                        that honour belongs to the LG G Watch R – but it’s by far the most useful. Its
-                        design may be pretty uninspiring and the display is fairly mediocre but the
-                        functionality afforded by the GPS is unrivalled in the Google smartwatch arena
-                        and the battery life is far better than we’ve seen on any of the Sony’s rivals.</td>
-                </tr>
-                <tr>
-                    <td>Features</td>
-                    <td>Size: 42mm</td>
-                    <td>Google Voice recognition; 1.2 GHz processor, 4GB of onboard storage, and 512MB of RAM</td>
-                    <td>Packs in GPS connectivity; sporty look</td>
-                </tr>
-                <tr>
-                    <td>Price</td>
-                    <td>$349</td>
-                    <td>$149.99</td>
-                    <td>from $112</td>
-                </tr>
-        </table><!--tabeli lõpp-->
+            <div class="wrapper">
+                <header>
+                    <h1><em>SMARTWATCH</em></h1>
+                    <p class="intro">Hello! My name is Rajur and I gathered here some information about smartwatches. It is an interesting toy but is it still worth to be purchased?</p>
+                </header>
+                <br>
+                <section>
+                    <h5>
+                        <a href="http://secularinvestor.com/google-will-introduce-smartwatch-june/5006">Between all the takeovers, Google has still found enough time to develop its own smartwatch together with LG</a>
+                    </h5>
+                    <img src="http://sproutmoney.com/files/2014/02/smartwatch.png"/>
+                    <hr>
+                    <h3>Smartwatch review</h3>
+                    <table border="1"><!--tabeli algus-->
+                        <tr>
+                            <th></th>
+                            <th>The basic Apple Watch</th>
+                            <th>Samsung Gear Live</th>
+                            <th>Sony SmartWatch 3</th>
+                        </tr>
+                        <tr>
+                            <td>Description</td>
+                            <td>Apple Watch is good, but better suited on the
+                                wrists of early adopters and boutique shop
+                                regulars. It's convenient but there's a learning
+                                curve you have to overcome and a high price that
+                                some people won't be able to get around.</td>
+                            <td>The Gear Live has a clean metal body, more features for the price than the LG watch,
+                                works with many Android phones (4.3 or later), has impressive Google Voice recognition,
+                                and could have an interesting variety of apps sooner than later. Terrible battery life;
+                                a display that doesn't work in bright sunlight; a strange card-based notification system
+                                that doesn't always pop up with the info you want; annoying snap-on charger that's not easy to attach.</td>
+                            <td>The Sony SmartWatch 3 might not be the best-looking Android Wear device so far –
+                                that honour belongs to the LG G Watch R – but it’s by far the most useful. Its
+                                design may be pretty uninspiring and the display is fairly mediocre but the
+                                functionality afforded by the GPS is unrivalled in the Google smartwatch arena
+                                and the battery life is far better than we’ve seen on any of the Sony’s rivals.</td>
+                        </tr>
+                        <tr>
+                            <td>Features</td>
+                            <td>Size: 42mm</td>
+                            <td>Google Voice recognition; 1.2 GHz processor, 4GB of onboard storage, and 512MB of RAM</td>
+                            <td>Packs in GPS connectivity; sporty look</td>
+                        </tr>
+                        <tr>
+                            <td>Price</td>
+                            <td>$349</td>
+                            <td>$149.99</td>
+                            <td>from $112</td>
+                        </tr>
+                    </table><!--tabeli lõpp-->
+                    <hr>
+                    <dl>
+                        <dt>Tea</dt>
+                        <dd>Tasty hot drink</dd>
+                        <dt>Juice</dt>
+                        <dd>Sugary cold drink</dd>
+                    </dl>
+                    <blockquote cite="http://www.ico.org/">
+                        The International Coffee Organization (ICO) is the main intergovernmental organization for coffee,
+                        bringing together exporting and importing Governments to tackle the challenges facing the world coffee
+                        sector through international cooperation. Its Member Governments represent 95% of world coffee production
+                        and 83% of world consumption. - See more at: http://www.ico.org/mission07_e.asp?section=About_Us#sthash.3mcNolOb.dpuf
+                    </blockquote>
+                    <p>The glass is either...</p>
+                    <del>Half- empty</del>
+                    <ins>Half- full</ins>
+                    <form>
+                        <textarea rows="4" cols="50">Codecademy is an education company.
+                        But not one in the way you might think. We're committed to building
+                        the best learning experience inside and out, making Codecademy the best
+                        place for our team to learn, teach, and create the online learning experience of the future.
+                        </textarea>
+                    </form>
 
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/s-84gk-KPOw" frameborder="0" allowfullscreen></iframe>
 
-
-
+                </section>
+                    <hr>
+                <footer>
+                    <p>Posted by: Rajur Hint</p>
+                    <address>Send me a letter: <a href="mailto:rajur.hint@khk.ee">Rajur Hint</a>
+                    <br>
+                    <p>Or visit my website:</p>
+                    <a href="https://sites.google.com/a/khk.ee/rajurh/"><abbr title="Rajur Hint">RH</abbr></a>
+                </footer>
+            </div>
 </body>
