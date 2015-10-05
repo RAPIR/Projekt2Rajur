7203157187989e868e9b21d69d5db4e0ef214066
Rajur <rajur.hint@gmail.com>
2015-10-05 10:48:48 +0100

    video lisatud + bg lis

--- index.php
+++ index.php
@@ -7,7 +7,11 @@
         <body>
             <div class="wrapper">
                 <header>
-                    <h1><em>SMARTWATCH</em></h1>
+                    <h1>
+                        <cite>
+                            <em>SMARTWATCH</em>
+                        </cite/>
+                    </h1>
                     <p class="intro">Hello! My name is Rajur and I gathered here some information about smartwatches. It is an interesting toy but is it still worth to be purchased?</p>
                 </header>
                 <br>
@@ -17,31 +21,43 @@
                     </h5>
                     <img src="http://sproutmoney.com/files/2014/02/smartwatch.png"/>
                     <hr>
+                    <main>
                     <h3>Smartwatch review</h3>
                     <table border="1"><!--tabeli algus-->
-                        <tr>
+                            <tr>
+                                <colgroup>
                             <th></th>
                             <th>The basic Apple Watch</th>
                             <th>Samsung Gear Live</th>
                             <th>Sony SmartWatch 3</th>
+                                </colgroup>
                         </tr>
                         <tr>
-                            <td>Description</td>
-                            <td>Apple Watch is good, but better suited on the
+                                <h4>
+                                    <td>Description</td>
+                                </h4>
+
+                            <dd>
+                                <td>Apple Watch is good, but better suited on the
                                 wrists of early adopters and boutique shop
                                 regulars. It's convenient but there's a learning
                                 curve you have to overcome and a high price that
                                 some people won't be able to get around.</td>
-                            <td>The Gear Live has a clean metal body, more features for the price than the LG watch,
+                            </dd>
+                            <td>
+                                <s>The Gear Live has a clean metal body, more features for the price than the LG watch,
                                 works with many Android phones (4.3 or later), has impressive Google Voice recognition,
                                 and could have an interesting variety of apps sooner than later. Terrible battery life;
                                 a display that doesn't work in bright sunlight; a strange card-based notification system
-                                that doesn't always pop up with the info you want; annoying snap-on charger that's not easy to attach.</td>
-                            <td>The Sony SmartWatch 3 might not be the best-looking Android Wear device so far –
+                                that doesn't always pop up with the info you want; annoying snap-on charger that's not easy to attach.</s>
+                            </td>
+                            <td>
+                                <section>The Sony SmartWatch 3 might not be the best-looking Android Wear device so far –
                                 that honour belongs to the LG G Watch R – but it’s by far the most useful. Its
                                 design may be pretty uninspiring and the display is fairly mediocre but the
                                 functionality afforded by the GPS is unrivalled in the Google smartwatch arena
-                                and the battery life is far better than we’ve seen on any of the Sony’s rivals.</td>
+                                and the battery life is far better than we’ve seen on any of the Sony’s rivals.</section>
+                            </td>
                         </tr>
                         <tr>
                             <td>Features</td>
@@ -56,21 +72,36 @@
                             <td>from $112</td>
                         </tr>
                     </table><!--tabeli lõpp-->
+                    </main>
                     <hr>
                     <dl>
+                        <i>
                         <dt>Tea</dt>
                         <dd>Tasty hot drink</dd>
                         <dt>Juice</dt>
                         <dd>Sugary cold drink</dd>
+                        </i>
                     </dl>
-                    <blockquote cite="http://www.ico.org/">
-                        The International Coffee Organization (ICO) is the main intergovernmental organization for coffee,
-                        bringing together exporting and importing Governments to tackle the challenges facing the world coffee
+                    <article>
+                        <del>
+                        <blockquote cite="http://www.ico.org/">
+                        <mark>
+                            <meta>The International Coffee Organization (ICO) is the main intergovernmental organization for coffee,
+                        bringing together <abbr>exporting<abbr/> and importing Governments to tackle the challenges facing the world coffee
                         sector through international cooperation. Its Member Governments represent 95% of world coffee production
                         and 83% of world consumption. - See more at: http://www.ico.org/mission07_e.asp?section=About_Us#sthash.3mcNolOb.dpuf
+                        </meta>
+                        </mark>
                     </blockquote>
+                        </del>
+                    </article>
+                    <q>
+                        <Säästa vett! Joo õlut!
+                    </q>
                     <p>The glass is either...</p>
-                    <del>Half- empty</del>
+                        <bdi>
+                            <del>Half- empty</del>
+                        </bdi>
                     <ins>Half- full</ins>
                     <form>
                         <textarea rows="4" cols="50">Codecademy is an education company.
@@ -78,6 +109,13 @@
                         the best learning experience inside and out, making Codecademy the best
                         place for our team to learn, teach, and create the online learning experience of the future.
                         </textarea>
+                        <menu>
+                            <ol>
+                            <li>Javascript</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            </ol>
+                        </menu>
                     </form>
 
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/s-84gk-KPOw" frameborder="0" allowfullscreen></iframe>
@@ -85,11 +123,16 @@
                 </section>
                     <hr>
                 <footer>
-                    <p>Posted by: Rajur Hint</p>
+                    <p>
+                        <h2>
+                        <code>Posted by: Rajur Hint</code>
+                        </h2>
+                    </p>
                     <address>Send me a letter: <a href="mailto:rajur.hint@khk.ee">Rajur Hint</a>
                     <br>
                     <p>Or visit my website:</p>
                     <a href="https://sites.google.com/a/khk.ee/rajurh/"><abbr title="Rajur Hint">RH</abbr></a>
+                        <details>Postikood</details>
                 </footer>
             </div>
 </body>
